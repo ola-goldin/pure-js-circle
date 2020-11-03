3883caa990234ddc42dfa186f6fad1b2ac304e26
ola-goldin <ola.kenig@gmail.com>
2020-11-03 22:00:33 +0200

    first commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,3 +1,5 @@
+
+ const item = document.getElementById("piece");
 // piece object
 const piece = (function() {
   let el = null;
@@ -19,26 +21,94 @@ function handleClick(ev) {
   piece.moveDelta(parseInt(this.dataset.dx), parseInt(this.dataset.dy));
 }
 
+function setRandomMovement(){
+  setResetBtn();
+  document.getElementById("random").addEventListener('click', evt=>{
+    let xy = getRandomPosition(item);
+    item.style.top = xy[0] + 'px';
+    item.style.left = xy[1] + 'px';
+  });
+}
+
+function setStyle(color){
+ item.style.backgroundColor = color;
+ item.onmouseenter = function(){
+  item.style.cssText = `background: transparent; border: 1px solid ${color};`;
+};
+item.onmouseleave = function(){
+  item.style.cssText = `background: ${color}; border: nine;`;
+};
+}
+
+function setColor(){
+window.fetch('http://dataservice.accuweather.com/currentconditions/v1/215854?apikey=GenwdR1gGdGLJJJA8cMbSEYJboRGlFqh')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      response.json().then(function(res) {
+        const {Temperature} = res[0];
+         if(Temperature.Metric.Value <0 && Temperature.Metric.Value >11){
+          setStyle('blue')
+         }
+         else if(Temperature.Metric.Value <=11 && Temperature.Metric.Value <=20){
+          setStyle('green')
+          }
+        
+         else if(Temperature.Metric.Value >=21 && Temperature.Metric.Value <=30){
+          setStyle('yellow')
+         }
+      
+         else if (Temperature.Metric.Value >30){
+          setStyle('red')
+         }
+         
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+ 
+}
+
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor((Math.random()*x)+50);
+	var randomY = Math.floor((Math.random()*y)+50);
+	return [randomX,randomY];
+}
+
+function setResetBtn(){
+  document.getElementById("reset").addEventListener('click', evt=>{
+    item.style.cssText = "top: 100px;left: 50%;"
+  });
+}
+
 function init() {
   const $btnUp = document.getElementById("btn-up");
-  $btnUp.dataset.dx = 0;
-  $btnUp.dataset.dy = -100;
-  $btnUp.addEventListener("click", handleClick);
   const $btnRight = document.getElementById("btn-right");
-  $btnRight.dataset.dx = 100;
-  $btnRight.dataset.dy = 0;
-  $btnRight.addEventListener("click", handleClick);
   const $btnDown = document.getElementById("btn-down");
-  $btnDown.dataset.dx = 0;
-  $btnDown.dataset.dy = 100;
-  $btnDown.addEventListener("click", handleClick);
   const $btnLeft = document.getElementById("btn-left");
-  $btnLeft.dataset.dx = -100;
-  $btnLeft.dataset.dy = 0;
-  $btnLeft.addEventListener("click", handleClick);
+ 
+  $btnUp.dataset.dx =  $btnRight.dataset.dy = $btnDown.dataset.dx =  $btnLeft.dataset.dy =0;
+  $btnUp.dataset.dy = $btnLeft.dataset.dx  = -100;
+  $btnRight.dataset.dx =  $btnDown.dataset.dy =100;
+
+  [$btnUp,$btnDown,$btnLeft,$btnRight].forEach(el=>{
+    el.addEventListener('click', handleClick) 
+  })
+  setResetBtn();
+  setRandomMovement();
+  setColor();
 }
 
 window.addEventListener("DOMContentLoaded", event => {
-  piece.init(document.getElementById("piece"));
+  piece.init(item);
   init();
 });
